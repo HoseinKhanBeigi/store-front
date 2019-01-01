94e387e8efac4841bc899357cd1ccec8c9f4f748
hosein <hoseinkhanbeigi@gmail.com>
2019-01-01 15:52:38 +0330

    add menuBarComponent

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
 /* @flow */
 
-import ErrorDisplay from './ErrorDisplay';
-import Loading from './Loading';
-import TopMenuBar from './TopMenuBar/TopMenuBar';
-import InputCheckBox from './CheckBox/InputCheckBox';
-import Paginations from './Pagination/Paginations';
-import SearchBox from './SearchBox/SearchBox';
-import SliderComponent from './Slider/SliderComponent';
-import Footer from './Footer/Footer';
+import ErrorDisplay from "./ErrorDisplay";
+import Loading from "./Loading";
+import TopMenuBar from "./TopMenuBar/TopMenuBar";
+import InputCheckBox from "./CheckBox/InputCheckBox";
+import Paginations from "./Pagination/Paginations";
+import SearchBox from "./SearchBox/SearchBox";
+import SliderComponent from "./Slider/SliderComponent";
+import Footer from "./Footer/Footer";
 
 export {
   ErrorDisplay,
---
+++ src/container/Sport/Sport.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+import React, { PureComponent } from 'react';
+import { compose } from 'redux';
+import { withRouter } from 'react-router-dom';
+
+type State = {
+  data: Array<String>,
+  onMous: Boolean
+};
+
+class Sport extends PureComponent<State> {
+  state = {
+    data: 'Sport'
+  };
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <h1>{data}</h1>
+      </div>
+    );
+  }
+}
+
+export default compose(withRouter)(Sport);
