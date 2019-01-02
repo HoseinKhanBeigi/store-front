3fc1b519e7412f4db98ad87804ee1d443caabfc7
hosein <hoseinkhanbeigi@gmail.com>
2019-01-03 03:25:41 +0330

    finish styling foor header

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,19 @@
 /* @flow */
 
-import ErrorDisplay from "./ErrorDisplay";
-import Loading from "./Loading";
-import TopMenuBar from "./TopMenuBar/TopMenuBar";
-import InputCheckBox from "./CheckBox/InputCheckBox";
-import Paginations from "./Pagination/Paginations";
-import SearchBox from "./SearchBox/SearchBox";
-import SliderComponent from "./Slider/SliderComponent";
-import Footer from "./Footer/Footer";
+import ErrorDisplay from './ErrorDisplay';
+import Loading from './Loading';
+import TopMenuBar from './TopMenuBar/TopMenuBar';
+import InputCheckBox from './CheckBox/InputCheckBox';
+import Paginations from './Pagination/Paginations';
+import SearchBox from './SearchBox/SearchBox';
+import SliderComponent from './Slider/SliderComponent';
+import Footer from './Footer/Footer';
+import Woman from './Woman/Woman';
 
 export {
   ErrorDisplay,
   Loading,
+  Woman,
   TopMenuBar,
   Footer,
   InputCheckBox,
