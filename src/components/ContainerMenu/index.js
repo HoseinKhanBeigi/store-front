351c7ee7fdd764ea1690e34a35ad6e8c31c0c463
hosein <hoseinkhanbeigi@gmail.com>
2019-01-03 16:06:51 +0330

    create Component for children of menu

---
+++ src/components/ContainerMenu/index.js
@@ -0,0 +1,6 @@
+import Woman from './Woman/Woman';
+import Men from './Men/Men';
+import Kids from './Kids/Kids';
+import Sport from './Sport/Sport';
+
+export { Woman, Men, Kids, Sport };
--- src/components/TopMenuBar/TopMenuBar.js
+++ src/components/TopMenuBar/TopMenuBar.js
@@ -1,12 +1,12 @@
 /* @flow */
 
-import React, { PureComponent } from 'react';
-import { Link } from 'react-router-dom';
-import { hot } from 'react-hot-loader';
-import { Icon } from 'antd';
-import SearchBox from '../SearchBox/SearchBox';
-import Woman from '../Woman/Woman';
-import logo from '../../theme/pic.png';
+import React, { PureComponent } from "react";
+import { Link } from "react-router-dom";
+import { hot } from "react-hot-loader";
+import { Icon } from "antd";
+import SearchBox from "../SearchBox/SearchBox";
+import { Woman, Men, Kids, Sport } from "../ContainerMenu";
+import logo from "../../theme/pic.png";
 
 type State = {
   data: String
@@ -14,7 +14,7 @@ type State = {
 
 class TopMenuBar extends PureComponent<State> {
   state = {
-    data: 'Home'
+    data: "Home"
   };
 
   render() {
@@ -31,7 +31,7 @@ class TopMenuBar extends PureComponent<State> {
             <li>
               <span>ثبت نام</span>
               <span>/</span>
-              <span>ورود</span>{' '}
+              <span>ورود</span>{" "}
             </li>
             <li>
               <span>سبد خرید</span>
@@ -46,111 +46,9 @@ class TopMenuBar extends PureComponent<State> {
             <div className="cbp-hsinner">
               <ul className="cbp-hsmenu">
                 <Woman />
-                <li>
-                  <Link to="">مردانه</Link>
-                  <ul className="cbp-hssubmenu">
-                    <li>
-                      <Link to="">
-                        <span>Lovely Slurp</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Lemony Grappa</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Eggy Liquor</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Fresh Juice</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Delicate Wine</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Fine Spirit</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Heavenly Ale</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Juicy Lemonade</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Wise Whiskey</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Sweet Rum</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Delicate Wine</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Fine Spirit</span>
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="">بچگانه</Link>
-                  <ul className="cbp-hssubmenu">
-                    <li>
-                      <Link to="">
-                        <span>Fresh Juice</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Sweet Rum</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Eggy Liquor</span>
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <Link to="">ورزشی</Link>
-                  <ul className="cbp-hssubmenu">
-                    <li>
-                      <Link to="">
-                        <span>Fresh Juicsdfsdfe</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Sweet sdfdsRum</span>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="">
-                        <span>Eggy Lisdsdfsquor</span>
-                      </Link>
-                    </li>
-                  </ul>
-                </li>
+                <Men />
+                <Kids />
+                <Sport />
               </ul>
             </div>
           </nav>
