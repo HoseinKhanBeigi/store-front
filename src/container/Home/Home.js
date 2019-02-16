aa169c89a4274774216b3f52a7ade09618da288a
hosein <hoseinkhanbeigi@gmail.com>
2019-02-16 15:18:23 +0330

    add home component

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -17,7 +17,41 @@ class Home extends PureComponent<State> {
   render() {
     const { data } = this.state;
 
-    return <div>home</div>;
+    const symbol = (
+      <polygon points="14.1333333 2.61666667 18.9333333 7.41666667 0 7.41666667 0 10.0833333 18.9333333 10.0833333 14.1333333 14.8833333 16 16.75 24 8.75 16 0.75" />
+    );
+
+    return (
+      <div>
+        <main>
+          <div className="grid grid--layout-1">
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item" />
+            <div className="grid__item grid__item--nav grid__item--nav-prev">
+              <svg className="icon icon--nav-arrow" viewBox="0 0 24 24">
+                {symbol}
+              </svg>
+            </div>
+            <div className="grid__item grid__item--nav grid__item--nav-next">
+              <svg className="icon icon--nav-arrow" viewBox="0 0 24 24">
+                {symbol}
+              </svg>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
   }
 }
 
