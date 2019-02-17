1316a571a44211e59031cd8fcd1c2dcc7aa5eed6
hosein <hoseinkhanbeigi@gmail.com>
2019-02-17 18:02:41 +0330

    create page home

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -11,11 +11,61 @@ type State = {
 
 class Home extends PureComponent<State> {
   state = {
-    data: "Home"
+    data: "Home",
+    hidden: true,
+    opacityValue: 0
+  };
+
+  componentDidMount() {
+    this.handleOpen();
+  }
+
+  handlePicesOpen = el => {
+    return new Promise((resolve, reject) => {
+      setTimeout(
+        () =>
+          resolve(
+            el.classList.remove("revealer--right"),
+            el.classList.remove("revealer--hideX"),
+            el.classList.add("revealer--left"),
+            el.classList.add("revealer--showX")
+          ),
+        500
+      );
+    });
+  };
+
+  handleOpen = async () => {
+    const newElementAll = document.querySelectorAll(".revealer");
+    for (var i = 0; i < newElementAll.length; i++) {
+      await this.handlePicesOpen(newElementAll[i]);
+    }
+  };
+
+  handlePicesClose = el => {
+    return new Promise((resolve, reject) => {
+      setTimeout(
+        () =>
+          resolve(
+            el.classList.remove("revealer--left"),
+            el.classList.remove("revealer--showX"),
+            el.classList.add("revealer--right"),
+            el.classList.add("revealer--hideX")
+          ),
+        500
+      );
+    });
+  };
+
+  handleClose = async () => {
+    const newElementAll = document.querySelectorAll(".revealer");
+    for (var i = 0; i < newElementAll.length; i++) {
+      await this.handlePicesClose(newElementAll[i]);
+    }
   };
 
   render() {
-    const { data } = this.state;
+    const { data, opacityValue } = this.state;
 
     const symbol = (
       <polygon points="14.1333333 2.61666667 18.9333333 7.41666667 0 7.41666667 0 10.0833333 18.9333333 10.0833333 14.1333333 14.8833333 16 16.75 24 8.75 16 0.75" />
@@ -23,30 +73,60 @@ class Home extends PureComponent<State> {
 
     return (
       <div>
+        <button
+          onClick={() => {
+            this.handleOpen();
+          }}
+        >
+          open
+        </button>
+        <button
+          onClick={() => {
+            this.handleClose();
+          }}
+        >
+          close
+        </button>
         <main>
           <div className="grid grid--layout-1">
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item" />
-            <div className="grid__item grid__item--nav grid__item--nav-prev">
-              <svg className="icon icon--nav-arrow" viewBox="0 0 24 24">
-                {symbol}
-              </svg>
+            <div className="grid__item">
+              <div className="revealer" />
             </div>
-            <div className="grid__item grid__item--nav grid__item--nav-next">
-              <svg className="icon icon--nav-arrow" viewBox="0 0 24 24">
-                {symbol}
-              </svg>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
+            </div>
+            <div className="grid__item">
+              <div className="revealer" />
             </div>
           </div>
         </main>
