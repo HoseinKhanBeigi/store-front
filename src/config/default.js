4535e04f20c1cbc0571d46eeaea3019d91556211
hosein <hoseinkhanbeigi@gmail.com>
2019-02-17 19:21:50 +0330

    fixed config for host

--- src/config/default.js
+++ src/config/default.js
@@ -1,16 +1,16 @@
 /* @flow */
 
 module.exports = {
-  host: process.env.NODE_HOST || '192.168.1.4', // Define your host from 'package.json'
+  host: process.env.NODE_HOST || "portfolio-hoseinkhanbeigi.herokuapp.com", // Define your host from 'package.json'
   port: process.env.PORT,
   app: {
-    htmlAttributes: { lang: 'en' },
-    title: 'Store front',
-    titleTemplate: 'Store front - %s',
+    htmlAttributes: { lang: "en" },
+    title: "Store front",
+    titleTemplate: "Store front - %s",
     meta: [
       {
-        name: 'description',
-        content: 'The best react universal starter boilerplate in the world.'
+        name: "description",
+        content: "The best react universal starter boilerplate in the world."
       }
     ]
   }
