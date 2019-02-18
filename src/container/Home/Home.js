73ce88b4c3ccaed5625b8864da275a24a24d3f55
hosein <hoseinkhanbeigi@gmail.com>
2019-02-19 02:16:05 +0330

    create portfolio

--- src/container/Home/Home.js
+++ src/container/Home/Home.js
@@ -1,8 +1,8 @@
 /* @flow */
 
-import React, { PureComponent } from "react";
-import { compose } from "redux";
-import { withRouter } from "react-router-dom";
+import React, { PureComponent } from 'react';
+import { compose } from 'redux';
+import { withRouter } from 'react-router-dom';
 
 type State = {
   data: Array<String>,
@@ -11,7 +11,7 @@ type State = {
 
 class Home extends PureComponent<State> {
   state = {
-    data: "Home",
+    data: 'Home',
     hidden: true,
     opacityValue: 0
   };
@@ -20,46 +20,45 @@ class Home extends PureComponent<State> {
     this.handleOpen();
   }
 
-  handlePicesOpen = el => {
-    return new Promise((resolve, reject) => {
+  handlePicesOpen = el =>
+    new Promise((resolve, reject) => {
       setTimeout(
         () =>
           resolve(
-            el.classList.remove("revealer--right"),
-            el.classList.remove("revealer--hideX"),
-            el.classList.add("revealer--left"),
-            el.classList.add("revealer--showX")
+            el.classList.remove('revealer--right'),
+            el.classList.remove('revealer--hideX'),
+            el.classList.add('revealer--left'),
+            el.classList.add('revealer--showX')
           ),
         500
       );
     });
-  };
 
   handleOpen = async () => {
-    const newElementAll = document.querySelectorAll(".revealer");
-    for (var i = 0; i < newElementAll.length; i++) {
+    this.setState({ opacityValue: 1 });
+    const newElementAll = document.querySelectorAll('.revealer');
+    for (let i = 0; i < newElementAll.length; i++) {
       await this.handlePicesOpen(newElementAll[i]);
     }
   };
 
-  handlePicesClose = el => {
-    return new Promise((resolve, reject) => {
+  handlePicesClose = el =>
+    new Promise((resolve, reject) => {
       setTimeout(
         () =>
           resolve(
-            el.classList.remove("revealer--left"),
-            el.classList.remove("revealer--showX"),
-            el.classList.add("revealer--right"),
-            el.classList.add("revealer--hideX")
+            el.classList.remove('revealer--left'),
+            el.classList.remove('revealer--showX'),
+            el.classList.add('revealer--right'),
+            el.classList.add('revealer--hideX')
           ),
         500
       );
     });
-  };
 
   handleClose = async () => {
-    const newElementAll = document.querySelectorAll(".revealer");
-    for (var i = 0; i < newElementAll.length; i++) {
+    const newElementAll = document.querySelectorAll('.revealer');
+    for (let i = 0; i < newElementAll.length; i++) {
       await this.handlePicesClose(newElementAll[i]);
     }
   };
@@ -88,45 +87,174 @@ class Home extends PureComponent<State> {
           close
         </button>
         <main>
-          <div className="grid grid--layout-1">
+          <div
+            className="grid grid--layout-1"
+            style={{ opacity: opacityValue }}
+          >
             <div className="grid__item">
-              <div className="revealer" />
+              <div className="revealer revealer--right revealer--hideX" />
+              <div
+                className="grim__item-inner"
+                style={{ opacity: 1, transform: `translateY(${0}%)` }}
+              >
+                <h3 className="grim__item-title">speakers</h3>
+                <span className="grim__item-desc">
+                  Smart people. All in one place.
+                </span>
+              </div>
             </div>
             <div className="grid__item">
-              <div className="revealer" />
+              <div className="revealer revealer--right revealer--hideX" />
+              <div
+                className="grim__item-inner"
+                style={{ opacity: 1, transform: `translateY(${0}%)` }}
+              >
+                <h3 className="grim__item-title">venue</h3>
+                <span className="grim__item-desc">One place for all.</span>
+              </div>
             </div>
             <div className="grid__item">
-              <div className="revealer" />
+              <div className="revealer revealer--right revealer--hideX" />
+              <div
+                className="grim__item-inner"
+                style={{ opacity: 1, transform: `translateY(${0}%)` }}
+              >
+                <h3 className="grim__item-title">hossein khan beigi</h3>
+                <span
+                  className="grim__item-desc"
+                  style={{
+                    fontFamily: 'monospace'
+                  }}
+                >
+                  javascript engineer specialist with 4+ experience at 2
+                  company.profetional experience in developing compelex Element
+                  in any web & webApp based on js-css-html with along tech like
+                  react-redux Vue-VueX angular. seeking to further career by
+                  growing with passion for the learning new things.
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      fontFamily: 'monospace',
+                      marginTop: '12px',
+                      fontSize: '16px'
+                    }}
+                  >
+                    <span> Address: tehran - iran</span>
+                    <span> Phone: +9809383345749</span>
+                    <span> E-mail: hoseinkhanbeigi@gmail.com</span>
+                  </div>
+                </span>
+              </div>
             </div>
             <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
-            </div>
-            <div className="grid__item">
-              <div className="revealer" />
+              <div className="revealer revealer--right revealer--hideX" />
+              <span style={{ fontFamily: 'monospace' }}>
+                javascript/html/css/sass
+              </span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '95%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>react/vue/angular</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '90%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>nodejs</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '50%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>webpack</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '70%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>mongoDB</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '40%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>
+                stateManagement(redux || any)
+              </span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '90%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>python/ruby</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '30%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>jest</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '70%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>
+                webSocket/RabbitMQ
+              </span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '70%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
+              <span style={{ fontFamily: 'monospace' }}>GraphQL Apollo</span>
+              <div
+                className="grid__item1"
+                style={{
+                  marginBottom: '12px',
+                  width: '70%'
+                }}
+              >
+                <div className="revealer revealer--right revealer--hideX" />
+              </div>
             </div>
           </div>
         </main>
